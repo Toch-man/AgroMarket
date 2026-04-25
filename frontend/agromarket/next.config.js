b31537de2d1665d1a6701f890a31d1d765e1edfe
tochukwu <okeakputochukwu9@gmail.com>
2026-04-25 10:22:10 -0400

    next config

--- frontend/agromarket/next.config.js
+++ frontend/agromarket/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: require("path").join(__dirname, "../../"),
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
