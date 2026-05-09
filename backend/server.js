7f4e3977335053df8bcb4d477993c2e1c0bcf91b
tochukwu <okeakputochukwu9@gmail.com>
2026-05-09 11:37:15 -0400

    auth test added

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+console.log(" SERVER STARTED");
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
